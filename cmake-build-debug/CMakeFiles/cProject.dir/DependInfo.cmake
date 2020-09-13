2e27e446edd045ede7ff6dd5c10bb1d115e6fb9b
xGaL <gal2109@gmail.com>
2020-09-13 18:27:25 +0300

    got rid of some mallocs

---
+++ cmake-build-debug/CMakeFiles/cProject.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/gal21/CLionProjects/projectApproach/divider.c" "C:/Users/gal21/CLionProjects/projectApproach/cmake-build-debug/CMakeFiles/cProject.dir/divider.c.obj"
+  "C:/Users/gal21/CLionProjects/projectApproach/main.c" "C:/Users/gal21/CLionProjects/projectApproach/cmake-build-debug/CMakeFiles/cProject.dir/main.c.obj"
+  "C:/Users/gal21/CLionProjects/projectApproach/spmat.c" "C:/Users/gal21/CLionProjects/projectApproach/cmake-build-debug/CMakeFiles/cProject.dir/spmat.c.obj"
+  "C:/Users/gal21/CLionProjects/projectApproach/stack.c" "C:/Users/gal21/CLionProjects/projectApproach/cmake-build-debug/CMakeFiles/cProject.dir/stack.c.obj"
+  "C:/Users/gal21/CLionProjects/projectApproach/utils.c" "C:/Users/gal21/CLionProjects/projectApproach/cmake-build-debug/CMakeFiles/cProject.dir/utils.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
